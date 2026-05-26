5a45e4d973d9fac7f9882c6a9bb95c6fb58cd52c
VickieAbdul <81722237+VickieAbdul@users.noreply.github.com>
2026-05-26 12:41:25 +0100

    Update Day-1.sql

--- Day-1.sql
+++ Day-1.sql
@@ -1,7 +1,7 @@
 -- Problem: Write a solution to find the ids of products that are both low fat and recyclable.
 -- Difficulty: Easy
 -- My approach: Simple WHERE filter with AND condition
--- LeetCode #1
+-- LeetCode #1757
 
 SELECT
     product_id
