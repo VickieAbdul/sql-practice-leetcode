946ea779216b1853870d49448115b2510761ab71
VickieAbdul <81722237+VickieAbdul@users.noreply.github.com>
2026-05-26 11:26:35 +0100

    Create Q2.sql

---
+++ Q2.sql
@@ -0,0 +1,11 @@
+-- Problem: Find the names of the customer that are either:referred by any customer with id != 2., not referred by any customer.
+-- Difficulty: Easy
+-- My approach: Simple WHERE filter
+-- LeetCode #2
+
+SELECT 
+    name
+FROM 
+    Customer
+WHERE 
+    referee_id != 2 OR referee_id IS NULL;
