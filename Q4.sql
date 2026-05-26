3c1f9373d401620c57d8f0e6b829372b36012386
VickieAbdul <81722237+VickieAbdul@users.noreply.github.com>
2026-05-26 12:02:17 +0100

    Create Q4.sql

---
+++ Q4.sql
@@ -0,0 +1,14 @@
+-- Problem : Write a solution to find all the authors that viewed at least one of their own articles. 
+-- Return the result table sorted by id in ascending order.
+-- Difficulty : Easy
+-- LeetCode #4
+-- My approach : Use DISTINCT to get the authors
+
+SELECT 
+    DISTINCT(author_id) AS id
+FROM 
+    Views
+WHERE 
+    author_id = viewer_id
+ORDER BY 
+    id ASC;
